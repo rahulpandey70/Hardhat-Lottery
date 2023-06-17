be984e6cf7bfcda0d4156c4453c5c6867429b2ea
rahulpandey70 <rahulpandey9103@gmail.com>
2023-06-17 17:01:59 +0530

    hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL ||
+    `https://eth-sepolia.g.alchemy.com/v2/${process.env.ETHERSCAN_API_KEY}`
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "0x"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
+module.exports = {
+    solidity: "0.8.18",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [SEPOLIA_PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        token: "MATIC",
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+}
