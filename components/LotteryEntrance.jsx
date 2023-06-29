bf854f21ad29bc10a95f15359acfa94f94fdef96
rahulpandey70 <rahulpandey9103@gmail.com>
2023-06-29 22:50:57 +0530

    add tailwind css

--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -1,20 +1,117 @@
-import { useWeb3Contract } from "react-moralis"
 import { abi, contractAddresses } from "../constants"
-import { useMoralis } from "react-moralis"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
+import { useNotification, Button } from "web3uikit"
 
 export default function LotteryEntrance() {
-    const { chainId: chainIdHex } = useMoralis()
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
     const chainId = parseInt(chainIdHex)
     const lotteryAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+    const [entranceFee, setEntranceFee] = useState("0")
+    const [numPlayers, setNumPlayers] = useState("0")
+    const [recentWinner, setRecentWinner] = useState("0")
+
+    const dispatch = useNotification()
 
     // Have a function to enter the lottery
-    const { runContractFunction: enterLottery } = useWeb3Contract({
+    const {
+        runContractFunction: enterLottery,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
         abi: abi,
         contractAddress: lotteryAddress,
         functionName: "enterLottery",
         params: {},
-        msgValue: "",
+        msgValue: entranceFee,
     })
 
-    return <div>LotteryEntrance</div>
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "getNumberOfPlayers",
+        params: {},
+    })
+
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "getRecentWinner",
+        params: {},
+    })
+
+    async function updateUi() {
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        const numPlayersFromCall = (await getNumberOfPlayers()).toString()
+        const recentWinnerFromCall = (await getRecentWinner()).toString()
+        setEntranceFee(entranceFeeFromCall)
+        setNumPlayers(numPlayersFromCall)
+        setRecentWinner(recentWinnerFromCall)
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUi()
+        }
+    }, [isWeb3Enabled])
+
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+        updateUi()
+    }
+
+    const handleNewNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+
+    return (
+        <div className="p-5">
+            <h1>Hi, From Lottery Entrance</h1>
+            {lotteryAddress ? (
+                <>
+                    <button
+                        onClick={async function () {
+                            await enterLottery({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.log(error),
+                            })
+                        }}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        disabled={isLoading || isFetching}
+                    >
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-6 w-6 border-b-2 rounded-full" />
+                        ) : (
+                            <div>Enter Lottery</div>
+                        )}
+                    </button>{" "}
+                    <br />
+                    <strong>Entrance Fee:</strong> {ethers.utils.formatUnits(entranceFee, "ether")}
+                    ETH
+                    <br />
+                    <strong>Players:</strong> {numPlayers}
+                    <br />
+                    <strong>RecentWinner:</strong> {recentWinner}
+                </>
+            ) : (
+                <div>No lottery address detected.</div>
+            )}
+            <div></div>
+        </div>
+    )
 }
